4f9b5796e60e99a23fe43d8ae7e220ffa2480212
Meehan Timothy <timothy.meehan@student.ecp.fr>
2020-10-27 11:02:10 +0100

    parrot

--- functions/parrot.js
+++ functions/parrot.js
@@ -1,11 +1,10 @@
 exports.handler = async function(event, context) {
-    console.log(event);
     var message = event.body.message;
     // if (!message) message= "Please enter a message !"
     
     return {
       statusCode:200,
-      body: JSON.stringify({ message:  "test" })
+      body: JSON.stringify(event)
     };
   };
   
