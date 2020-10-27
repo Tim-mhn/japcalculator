81a97d7dc893a42b03b58dc9fc457ba24673ad04
Meehan Timothy <timothy.meehan@student.ecp.fr>
2020-10-27 11:37:56 +0100

    Added multiplication derivative and test

--- functions/parrot.js
+++ functions/parrot.js
@@ -15,7 +15,7 @@ exports.handler = async function(event, context) {
     
     return {
       statusCode:200,   
-      body: JSON.stringify({ "message": message ?})
+      body: JSON.stringify({ "message": message})
     };
   };
   
